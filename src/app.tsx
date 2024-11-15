c1e7b79f1ba0d63381dcd9fb1fcb8aa1ade229a1
notjustanyjoe <josephgsalas@gmail.com>
2024-11-14 20:07:55 -0700

    add in pieces to make the pbs ui work with dummy data.

--- src/app.tsx
+++ src/app.tsx
@@ -1,48 +1,65 @@
-/*
- * This file is part of Cockpit.
- *
- * Copyright (C) 2017 Red Hat, Inc.
- *
- * Cockpit is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * Cockpit is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
- */
 
 import React, { useEffect, useState } from 'react';
 import { Alert } from "@patternfly/react-core/dist/esm/components/Alert/index.js";
 import { Card, CardBody, CardTitle } from "@patternfly/react-core/dist/esm/components/Card/index.js";
-
+import {
+    Page,
+    PageSection,
+    PageSectionVariants,
+    Title,
+    Split,
+    SplitItem,
+  } from '@patternfly/react-core';
+import { PBSJob, ClusterResource } from './types/pbs';
+import { JobsTable } from './components/JobsTable';
+import { ClusterStatus } from './components/ClusterStatus';
+import { fetchJobs, fetchClusterResources } from './services/pbsService';
 import cockpit from 'cockpit';
 
 const _ = cockpit.gettext;
 
 export const Application = () => {
     const [hostname, setHostname] = useState(_("Unknown"));
-
+    const [jobs, setJobs] = useState<PBSJob[]>([]);
+    const [resources, setResources] = useState<ClusterResource[]>([]);
     useEffect(() => {
         const hostname = cockpit.file('/etc/hostname');
         hostname.watch(content => setHostname(content?.trim() ?? ""));
         return hostname.close;
     }, []);
 
+    useEffect(() => {
+        const loadData = async () => {
+          const [jobsData, resourcesData] = await Promise.all([
+            fetchJobs(),
+            fetchClusterResources(),
+          ]);
+          setJobs(jobsData);
+          setResources(resourcesData);
+        };
+    
+        loadData();
+        const interval = setInterval(loadData, 30000); // Refresh every 30 seconds
+    
+        return () => clearInterval(interval);
+    }, []);      
     return (
-        <Card>
-            <CardTitle>Starter Kit</CardTitle>
+        <><Card>
+            <CardTitle>PBS Cluster Monitor</CardTitle>
             <CardBody>
                 <Alert
                     variant="info"
-                    title={ cockpit.format(_("Running on $0"), hostname) }
-                />
+                    title={cockpit.format(_("Running on $0"), hostname)} />
             </CardBody>
         </Card>
+        <Page>
+            <PageSection>
+                <ClusterStatus resources={resources} />
+            </PageSection>
+            <PageSection>
+                <JobsTable jobs={jobs} />
+            </PageSection>
+        </Page>
+        </>
     );
 };
